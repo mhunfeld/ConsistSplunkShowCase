107bff30ff8d8dda3967483a114a1ebfa388f6e7
ximuva02@gmail.com <ximuva02@gmail.com>
2020-07-06 17:13:56 +0200

    Missing js and numberOfRows Token

--- appserver/static/extensions/table/main.js
+++ appserver/static/extensions/table/main.js
@@ -2,8 +2,18 @@ require([
     '/static/app/ConsistSplunkToolbox/extensions/table/src/table.js'
 ], function(Table){
 
+    new Table('exampleTable')
+        .addNumberOfTableRowsToggler('exampleTable_numberOfRowsToken')
+        .addColumnChooser({
+            inputfieldId: 'exampleFields',
+            sortable: true,
+            cookieName: 'exampleTableColumns'
+        });
 
-    new Table('table');
+
+    new Table('fixedColumnTable')
+        .addNumberOfTableRowsToggler('fixedColumnTable_numberOfRowsToken')
+        .addFixedColumns(2);
 });
 
 //@ sourceURL=table_main.js
