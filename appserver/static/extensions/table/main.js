f3700998780dc90748861e96387fbd662c489a5e
ximuva02@gmail.com <ximuva02@gmail.com>
2020-07-13 21:39:00 +0200

    readme and renameHeader example added

--- appserver/static/extensions/table/main.js
+++ appserver/static/extensions/table/main.js
@@ -1,6 +1,11 @@
 require([
-    '/static/app/ConsistSplunkToolbox/extensions/table/src/table.js'
-], function(Table){
+    '/static/app/ConsistSplunkToolbox/extensions/table/src/table.js',
+    '/static/app/ConsistSplunkShowCase/helper/ReadMeView.js',
+    'splunkjs/mvc/simplexml/ready!'
+], function(Table, ReadMeView) {
+
+    new Table('renameTableHeader')
+        .renameHeader('extra long descriptionalble field name', 'field 2')
 
     new Table('exampleTable')
         .addNumberOfTableRowsToggler('exampleTable_numberOfRowsToken')
@@ -14,6 +19,13 @@ require([
     new Table('fixedColumnTable')
         .addNumberOfTableRowsToggler('fixedColumnTable_numberOfRowsToken')
         .addFixedColumns(2);
+
+
+    var readMeView = new ReadMeView({
+        path: '/ConsistSplunkToolbox/extensions/table'
+    });
+
+    $('#readme').append(readMeView.render().$el);
 });
 
 //@ sourceURL=table_main.js
