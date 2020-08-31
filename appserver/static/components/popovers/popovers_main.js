2b28dccab3ababe1089361989d0758982a3acef4
hunfeld@consist.de <hunfeld@consist.de>
2020-08-31 16:56:10 +0200

    Comments in popover example

--- appserver/static/components/popovers/popovers_main.js
+++ appserver/static/components/popovers/popovers_main.js
@@ -8,6 +8,8 @@ require([
 
         popovers.initAll();
 
+        //Unterstützung von Themes muss bei initialisierung in JS manuell implementiert werden.
+        //statt content wird ein template übergeben, in dem die css-Klasse für das Theming übergeben werdne könnte.
         $('#popoverInJS').popover({
             title:"See What I Did There?",
             placement:"top",
@@ -15,6 +17,11 @@ require([
             content:"init in JS!!! :) "
         });
 
+
+
+
+    //Unterstützung von Themes muss bei initialisierung in JS manuell implementiert werden.
+    //statt content wird ein template übergeben, in dem die css-Klasse für das Theming übergeben werdne könnte.
         var field1 = mvc.Components.get('field1');
         field1.$el.find('label').popover({
             title:"See What I Did There?",
@@ -22,5 +29,9 @@ require([
             trigger:"hover",
             content:"init in JS! :) "
         }).append('<span class="icon-info-circle info"></span>');
+
+
+
+
 
     });
